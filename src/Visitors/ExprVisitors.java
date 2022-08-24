b20f025a7db3f26c59e561aab70434dd8c69acad
Gebi <andreigebaila@gmail.com>
2022-08-24 23:41:59 +0100

    Moved methods out of this class

--- src/Visitors/ExprVisitors.java
+++ src/Visitors/ExprVisitors.java
@@ -4,10 +4,12 @@ import MainPackage.*;
 import java.util.Objects;
 
 public class ExprVisitors extends gebLBaseVisitor<Value>{
+
     @Override
     public Value visitString(gebLParser.StringContext ctx){
         return new Value(ctx.STRING_VAL().getText());
     }
+
     @Override
     public Value visitNum(gebLParser.NumContext ctx){
         Value value = new Value(Float.parseFloat(ctx.NUM().getText()));
@@ -15,10 +17,11 @@ public class ExprVisitors extends gebLBaseVisitor<Value>{
             value.isInt = true;
         return value;
     }
+
     @Override
     public Value visitId(gebLParser.IdContext ctx){
         Value var = new Value();
-        Value val = Evaluator.findVar(ctx.ID().getText(), ctx.start.getLine());
+        Value val = VarHandler.findVar(ctx.ID().getText());
         if(val == null)
             Evaluator.ThrowError(1, ctx.start.getLine(), ctx.ID().getText(), 0, 0);
         else{
@@ -28,10 +31,12 @@ public class ExprVisitors extends gebLBaseVisitor<Value>{
         }
         return var;
     }
+
     @Override
     public Value visitParenthesis(gebLParser.ParenthesisContext ctx){
         return this.visit(ctx.operation());
     }
+
     @Override
     public Value visitMultOp(gebLParser.MultOpContext ctx){
         Value l = this.visit(ctx.operation(0)), r = this.visit(ctx.operation(1));
@@ -41,6 +46,7 @@ public class ExprVisitors extends gebLBaseVisitor<Value>{
             case gebLParser.MOD  -> { return new Value(l.getFloat() % r.getFloat());}
             default ->              { Evaluator.ThrowError(2, ctx.start.getLine(), "", 0, 0); return new Value();}}
     }
+
     @Override
     public Value visitAddOp(gebLParser.AddOpContext ctx){
         Value l = this.visit(ctx.operation(0)), r = this.visit(ctx.operation(1));
@@ -49,6 +55,7 @@ public class ExprVisitors extends gebLBaseVisitor<Value>{
             case gebLParser.SUB -> { return new Value(l.getFloat() - r.getFloat()); }
             default ->             { Evaluator.ThrowError(2, ctx.start.getLine(), "", 0, 0); return new Value();}}
     }
+
     @Override
     public Value visitComparisonExpression(gebLParser.ComparisonExpressionContext ctx){
         Value l = this.visit(ctx.operation(0)), r = this.visit(ctx.operation(1));
